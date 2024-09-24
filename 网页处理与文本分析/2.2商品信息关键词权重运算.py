805056fab6c94b919427eea8f4e63a8396689988
Rin_laurant <Rin_laurant@mywin.com>
2024-09-24 21:22:34 +0800

    补充完成了2.2注释

--- 网页处理与文本分析/2.2商品信息关键词权重运算.py
+++ 网页处理与文本分析/2.2商品信息关键词权重运算.py
@@ -28,7 +28,11 @@ class Solution:
         product_info = resp.text #保存商品信息
 
         # 用jieba库进行分词
-        results = analyse.extract_tags(product_info,topK = 5, allowPOS=('n','nr','ns')) # 这是官方文档给出的求tf-idf值的函数，
+        results = analyse.extract_tags(product_info,topK = 5, allowPOS=('n','nr','ns')) # 这是官方文档给出的求tf-idf值的函数，请参考https://github.com/fxsjy/jiebajieba.analyse.extract_tags(sentence, topK=20, withWeight=False, allowPOS=())
+        # sentence 为待提取的文本
+        # topK 为返回几个 TF/IDF 权重最大的关键词，默认值为 20
+        #withWeight 为是否一并返回关键词权重值，默认值为 False
+        #allowPOS 仅包括指定词性的词，默认值为空，即不筛选
         return results
 
         pass
